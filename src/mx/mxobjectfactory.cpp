d78f7065fb9b3137ab4748d7fcd6a70ba90d64ea
itsmattkc <itsmattkc@gmail.com>
2019-10-21 01:37:44 +1100

    improved lego/mxobjectfactory accuracy

--- src/mx/mxobjectfactory.cpp
+++ src/mx/mxobjectfactory.cpp
@@ -2,18 +2,18 @@
 
 #include "custom/debug.h"
 
-MxObjectFactory::MxObjectFactory() :
-    unk8_("MxPresenter", Unk_LookupMode0),
-    unkC_("MxCompositePresenter", Unk_LookupMode0),
-    unk10_("MxVideoPresenter", Unk_LookupMode0),
-    unk14_("MxFlcPresenter", Unk_LookupMode0),
-    unk18_("MxSmkPresenter", Unk_LookupMode0),
-    unk1C_("MxStillPresenter", Unk_LookupMode0),
-    unk20_("MxWavePresenter", Unk_LookupMode0),
-    unk24_("MxMIDIPresenter", Unk_LookupMode0),
-    unk28_("MxEventPresenter", Unk_LookupMode0),
-    unk2C_("MxLoopingFlcPresenter", Unk_LookupMode0),
-    unk30_("MxLoopingSmkPresenter", Unk_LookupMode0),
-    unk34_("MxLoopingMIDIPresenter", Unk_LookupMode0)
+MxObjectFactory::MxObjectFactory()
 {
+    unk8_ = MxAtomId("MxPresenter", Unk_LookupMode0);
+    unkC_ = MxAtomId("MxCompositePresenter", Unk_LookupMode0);
+    unk10_ = MxAtomId("MxVideoPresenter", Unk_LookupMode0);
+    unk14_ = MxAtomId("MxFlcPresenter", Unk_LookupMode0);
+    unk18_ = MxAtomId("MxSmkPresenter", Unk_LookupMode0);
+    unk1C_ = MxAtomId("MxStillPresenter", Unk_LookupMode0);
+    unk20_ = MxAtomId("MxWavePresenter", Unk_LookupMode0);
+    unk24_ = MxAtomId("MxMIDIPresenter", Unk_LookupMode0);
+    unk28_ = MxAtomId("MxEventPresenter", Unk_LookupMode0);
+    unk2C_ = MxAtomId("MxLoopingFlcPresenter", Unk_LookupMode0);
+    unk30_ = MxAtomId("MxLoopingSmkPresenter", Unk_LookupMode0);
+    unk34_ = MxAtomId("MxLoopingMIDIPresenter", Unk_LookupMode0);
 }
